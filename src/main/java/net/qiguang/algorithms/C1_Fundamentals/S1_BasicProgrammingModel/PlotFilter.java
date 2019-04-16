fd258ad60c227e5107a6bb7617a34285d6db62f1
hvn <hvn@elca.vn>
2019-04-16 18:46:54 +0700

    1.5 Input and Output

--- src/main/java/net/qiguang/algorithms/C1_Fundamentals/S1_BasicProgrammingModel/PlotFilter.java
+++ src/main/java/net/qiguang/algorithms/C1_Fundamentals/S1_BasicProgrammingModel/PlotFilter.java
@@ -1,7 +1,9 @@
 package net.qiguang.algorithms.C1_Fundamentals.S1_BasicProgrammingModel;
 
 import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdIn;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 /******************************************************************************
  *  Compilation:  javac PlotFilter.java
@@ -18,29 +20,40 @@ public class PlotFilter {
 
     public static void main(String[] args) {
 
-        // read in bounding box and rescale
-        double x0 = StdIn.readDouble();
-        double y0 = StdIn.readDouble();
-        double x1 = StdIn.readDouble();
-        double y1 = StdIn.readDouble();
-        StdDraw.setXscale(x0, x1);
-        StdDraw.setYscale(y0, y1);
+        try (FileInputStream is = new FileInputStream(new File("src\\main\\java\\net\\qiguang\\algorithms\\C1_Fundamentals\\S1_BasicProgrammingModel\\USA.txt"))) {
+            System.setIn(is);
 
-        // for bigger points
-        StdDraw.setPenRadius(0.005);
+            // read in bounding box and rescale
+            double x0 = StdIn.readDouble();
+            double y0 = StdIn.readDouble();
+            double x1 = StdIn.readDouble();
+            double y1 = StdIn.readDouble();
+            StdDraw.setXscale(x0, x1);
+            StdDraw.setYscale(y0, y1);
 
-        // to speed up performance, defer displaying points
-        StdDraw.enableDoubleBuffering();
+            // for bigger points
+            StdDraw.setPenRadius(0.005);
 
-        // plot points, one at a time
-        while (!StdIn.isEmpty()) {
-            double x = StdIn.readDouble();
-            double y = StdIn.readDouble();
-            StdDraw.point(x, y);
+            // to speed up performance, defer displaying points
+            StdDraw.enableDoubleBuffering();
+
+            // plot points, one at a time
+            while (!StdIn.isEmpty()) {
+                double x = StdIn.readDouble();
+                double y = StdIn.readDouble();
+                StdDraw.point(x, y);
+            }
+
+            // display all of the points now
+            StdDraw.show();
+        } catch (Exception e) {
+            StdOut.println(e);
         }
 
-        // display all of the points now
-        StdDraw.show();
+
+    }
+
+    private static void run(String[] args) {
 
     }
 }
