79bf43bdde34de28666a0655aeeb8337b299914a
hvn <hvn@elca.vn>
2019-06-21 15:47:13 +0700

    Qbselect (10)

--- src/main/java/net/qiguang/algorithms/C1_Fundamentals/S1_BasicProgrammingModel/Qbselect.java
+++ src/main/java/net/qiguang/algorithms/C1_Fundamentals/S1_BasicProgrammingModel/Qbselect.java
@@ -9,13 +9,13 @@ public class Qbselect {
 
     // get bit (of number status) at (row - 1)   |  row : 1 -> 4
     static int getbit(int row, int status) {
-        return (status >> (row - 1)) & 1;
+        return (status >> (row)) & 1;
     }
 
     static int ok(int status1, int status2) {
         int[] bit = new int[5];
         int[] elsebit = new int[5];
-        for (int row = 1; row <= 4; row++) {
+        for (int row = 0; row < 4; row++) {
             bit[row] = getbit(row, status1);
             elsebit[row] = getbit(row, status2);
             if ((bit[row] & elsebit[row]) == 1) {
@@ -28,37 +28,37 @@ public class Qbselect {
     static int value(int status, int col) {
         int[] bit = new int[5];
         int sum = 0;
-        for (int row = 1; row <= 4; row++) {
+        for (int row = 0; row < 4; row++) {
             bit[row] = getbit(row, status);
             if (bit[row] == 1) {
-                sum += arr[row - 1][col - 1];
+                sum += arr[row][col - 1];
             }
         }
         return sum;
     }
 
     static int bitmask() {
-        int[][] f = new int[9][n + 1];
-        int[][] fr = new int[9][9];
+        int[][] f = new int[8][n + 1];
+        int[][] fr = new int[8][8];
         int[] statuses = {0, 1, 2, 4, 5, 8, 9, 10};
         int res = MIN;
-        for (int i = 1; i <= 8; i++) {
-            for (int j = 1; j <= 8; j++) {
-                fr[i][j] = ok(statuses[i - 1], statuses[j - 1]);
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                fr[i][j] = ok(statuses[i], statuses[j]); // fr : 0 -> 8
             }
         }
         for (int col = 1; col <= n; col++) {
-            for (int i = 1; i <= 8; i++) {
+            for (int i = 0; i < 8; i++) {
                 int t = MIN;
-                for (int j = 1; j <= 8; j++) {
+                for (int j = 0; j < 8; j++) {
                     if (fr[i][j] == 1 && f[j][col - 1] > t) {
                         t = f[j][col - 1];
                     }
                 }
-                f[i][col] = t + value(statuses[i - 1], col);
+                f[i][col] = t + value(statuses[i], col);
             }
         }
-        for (int i = 1; i <= 8; i++) {
+        for (int i = 0; i < 8; i++) {
             res = Math.max(res, f[i][n]);
         }
         return res;
